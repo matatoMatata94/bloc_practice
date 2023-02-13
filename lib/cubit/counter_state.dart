86aae22460f8bf9697bf0999a73d560bc9a5948a
matatoMatata94 <73385202+matatoMatata94@users.noreply.github.com>
2023-02-13 22:19:00 +0100

    implemented some testing
    Created counter_cubit_test and counter_state_test

--- lib/cubit/counter_state.dart
+++ lib/cubit/counter_state.dart
@@ -1,6 +1,11 @@
-class CounterState {
+import 'package:equatable/equatable.dart';
+
+class CounterState extends Equatable {
   int counterValue;
   bool? wasIncremented;
 
   CounterState({required this.counterValue, this.wasIncremented});
+
+  @override
+  List<Object?> get props => [counterValue, wasIncremented];
 }
